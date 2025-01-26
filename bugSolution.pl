2b3bb1868bc436f11f2bf3253096cd941f34a241
BugHunter-X <askzenai@gmail.com>
2025-01-26 06:14:05 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3); foreach my $i (0..$#array) { print "Element $i: ", $array[$i], "\n"; } my @array = (1, 2, 3); for my $i (0..$#array){ print "Element $i: ", $array[$i], "\n"; }
